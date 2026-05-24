bd74df8d92ae161984e5e4c131854c2683c1dff6
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-24 17:07:23 +0600

    sorting in ascending and descending order

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-
+from django.db.models import Avg, Max, Min
 from .models import Book 
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating")) # rating__avg
+    print(f"avg rating: {avg_rating}")
     return render(request,"book_outlets/index.html", {
-        "books":books
+        "books":books, 
+        "total_number_of_books":num_books,
+        "average_rating":avg_rating
     })
 
 def book_detail(request, slug):
