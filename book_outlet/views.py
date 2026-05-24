77543961dd1d71f85d2e20609bf2dd75333c5f37
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-24 15:55:31 +0600

    Viewing data in templates

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Book 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+    return render(request,"book_outlets/index.html", {
+        "books":books
+    })
