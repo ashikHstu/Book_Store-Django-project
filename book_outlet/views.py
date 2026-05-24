f85aaeefa7ce7de2ffc0632615cc2673a8e7bb32
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-24 16:25:22 +0600

    url for each book added

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book 
 # Create your views here.
@@ -8,4 +9,19 @@ def index(request):
     books = Book.objects.all()
     return render(request,"book_outlets/index.html", {
         "books":books
+    })
+
+def book_detail(request, id):
+    
+    # try:
+    #     book =Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+
+    book = get_object_or_404(Book, id=id) # can use pk = id as well, pk is primary key
+    return render(request, "book_outlets/book_detail.html", {
+        "title": book.title, 
+        "author":book.author, 
+        "rating":book.rating, 
+        "is_bestseller":book.is_bestselling
     })
