e273b16e4463fe9d4a997ceed65bd0fee24d7e29
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-25 12:38:43 +0600

    Configuration admin settings

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,4 +4,8 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    # readonly_fields = ("slug",)
+
+admin.site.register(Book, BookAdmin)
