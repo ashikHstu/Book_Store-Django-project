e06bf438382e19d6ec5b0138ed17860b7bec5b8b
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-25 12:56:26 +0600

    admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -6,6 +6,8 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
     # readonly_fields = ("slug",)
 
 admin.site.register(Book, BookAdmin)
